0719c31244c248c5f347dc89a4512ce7514a33ac
Alex Wine <alexwine36@gmail.com>
2025-03-20 16:55:17 -0600

    fix added util lib

--- apps/web/registry/new-york/common/components/copy-add-button.tsx
+++ apps/web/registry/new-york/common/components/copy-add-button.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { getUrl } from '@repo/utils';
 import { CopyButton } from '../../copy-to-clipboard/components/copy-button';
 
 type CopyAddButtonProps = {
@@ -7,7 +7,7 @@ type CopyAddButtonProps = {
 } & React.ComponentProps<typeof CopyButton>['buttonProps'];
 
 export function CopyAddButton({ name, ...props }: CopyAddButtonProps) {
-  const value = `pnpm dlx shadcn@latest add ${window.location.origin}/r/${name}.json`;
+  const value = `pnpm dlx shadcn@latest add ${getUrl(`/r/${name}.json`)}`;
 
   return <CopyButton buttonProps={props} value={value} />;
 }
